0b046c3a57f87b44aeda63a3e199056533ad5a3f
lgb <cavin12345@naver.com>
2023-08-17 11:50:45 +0900

    1st distribute

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(cez*l^thn7m@**9rg0lpc@x#620zymalrcpups!xgd+e+!i0i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com', 'www.memozzang.com']
 
 
 # Application definition
